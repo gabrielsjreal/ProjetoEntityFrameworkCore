005a3b87861d0b8576bd6caa8b59a59b85c39016
Gabriel Souza <gabrielsjreal>
2019-06-18 15:29:33 -0300

    Add alguns comandos de Fluent API

--- ProjetoEntityFramework/ApplicationContext.cs
+++ ProjetoEntityFramework/ApplicationContext.cs
@@ -15,6 +15,18 @@ namespace ProjetoEntityFramework
 
         }
 
+        // comando para usar o Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //O Fluent API serve para especificar critérios relacionados aos atributos do banco
+            //Como por exemplo: Especificar o tamanho mx e min de campo da tabela no banco
+
+            // comando para definir o nome da tabela
+            modelBuilder.Entity<Produto>().ToTable("Produto");
+            //comando para definir o tam. max. do campo - Nome
+            modelBuilder.Entity<Produto>().Property(p => p.Nome).HasMaxLength(48);
+        }
+
         public DbSet<Categoria> Categoria { get; set; }
         public DbSet<Produto> Produto { get; set; }
     }
---
+++ ProjetoEntityFramework/Migrations/20190618182343_alterandoCampoENomeDeProduto.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoEntityFramework.Migrations
+{
+    public partial class alterandoCampoENomeDeProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produto",
+                maxLength: 48,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produto",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 48,
+                oldNullable: true);
+        }
+    }
+}
